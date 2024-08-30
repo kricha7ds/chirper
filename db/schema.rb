bd687fd2fed5c70768840154d8212e165d324ea0
Katherine Richards <kricha7ds@gmail.com>
2024-08-29 18:57:57 -0600

    add Tweet model

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 0) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_30_005017) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "tweets", force: :cascade do |t|
+    t.text "body"
+    t.integer "likes"
+    t.integer "retweets"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
